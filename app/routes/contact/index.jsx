56dc976901f48cf0503d8f3f04bb5b056f61f4db
Janderson1924 <justinanderson101@gmail.com>
2022-10-11 18:54:20 -0600

    define routes, update favicon

---
+++ app/routes/contact/index.jsx
@@ -0,0 +1,10 @@
+import { Link } from '@remix-run/react'
+
+export default function Contact() {
+  return (
+    <main>
+      <h1>CONTACT</h1>
+      <Link to='/'>Home</Link>
+    </main>
+  )
+}
---
+++ app/routes/projects/index.jsx
@@ -0,0 +1,10 @@
+import { Link } from '@remix-run/react'
+
+export default function Projects() {
+  return (
+    <main>
+      <h1>PROJECTS</h1>
+      <Link to='/'>Home</Link>
+    </main>
+  )
+}
