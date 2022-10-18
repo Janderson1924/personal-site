334da725ddbcb8668ed58c80f548ac13aea66a60
Janderson1924 <justinanderson101@gmail.com>
2022-10-18 13:53:50 -0600

    implement theme colors, test

---
+++ src/assets/theme/index.jsx
@@ -0,0 +1,26 @@
+import { createTheme } from '@mui/material/styles'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      // Purples
+      light: '#4D194D',
+      main: '#3E1F47',
+      dark: '#312244'
+    },
+    secondary: {
+      // Greens
+      light: '#006466',
+      main: '#0b525b',
+      dark: '#1b3a4b'
+    },
+    font: {
+      // Font Colors (eggshell, purple(light/dark))
+      light: '#e8e1e9',
+      main: '#f0ead6',
+      dark: '#c6b5c9'
+    }
+  }
+})
+
+module.exports = { theme }
