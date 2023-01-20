21dd79a41948d1a2ebdc4a33a5bf5f1876bd6cfb
Janderson1924 <justinanderson101@gmail.com>
2023-01-20 15:31:06 -0700

    add cypress for testing, configure

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    'video': false,
+    'screenshotOnRunFailure': false,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    }
+  }
+})
